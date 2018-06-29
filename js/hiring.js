49812f0f88895ed9d4f8556f635aab3492f98c4a
Jun Hong <junhong@Juns-MacBook-Pro.local>
2018-06-28 23:22:49 -0700

    update css

--- js/hiring.js
+++ js/hiring.js
@@ -1,22 +1,15 @@
 $(function () {
-
-    // 点击导航操作
     $("a.link_item").click(function () {
         window.location.href = './index.html?name=' + $(this).attr("href").substring(1, $(this).attr("href").length);
     });
 
     if(/Android|webOS|iPhone|iPod|iPad|BlackBerry/i.test(navigator.userAgent) && window.orientation === 0) {
-       // 手机端初始化
         $("header .delete").css('display', 'none');
         $("header .links").css('display', 'none');
     }
-
-    // 监听屏幕旋转
     $(window).bind('orientationchange', function(e){
         window.location.reload();
     });
-
-    // 获取屏幕可见区域高度
     function getClientHeight() {
         var clientHeight = 0;
         if (document.body.clientHeight && document.documentElement.clientHeight) {
@@ -30,33 +23,27 @@ $(function () {
         }
         return clientHeight;
     }
-
-    // 展示menu
     function showMenu() {
         $("header .menu").css('display', 'block');
         $("header .links").css('display', 'none');
-        // 显示icon
         $("header .icon").animate({
             opacity: 1
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 显示menu
         $("header .menu").animate({
             opacity: 1
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 隐藏delete
         $("header .delete").animate({
             opacity: 0
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 隐藏links
         $("header .links").animate({
             opacity: 0
         }, {
@@ -68,34 +55,29 @@ $(function () {
         }, 600);
     }
 
-    // 隐藏menu
     function hideMenu() {
         $("header .delete").css('display', 'block');
         $("header .delete").css('opacity', 0);
         $("header .delete").css('z-index', 100);
         $("header .links").css('display', 'flex');
-        // 隐藏icon
         $("header .icon").animate({
             opacity: 0
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 隐藏menu
         $("header .menu").animate({
             opacity: 0
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 显示delete
         $("header .delete").animate({
             opacity: 1
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 显示links
         $("header .links").animate({
             opacity: 1
         }, {
@@ -107,19 +89,16 @@ $(function () {
         }, 600);
     }
 
-    // 手机端点击 右上角菜单
     $("header .menu").click(function () {
         hideMenu();
         return false;
     });
 
-    // 手机端点击 右上角关闭菜单
     $("header .delete").click(function () {
         showMenu();
         return false;
     });
 
-    // 初始化swiper
     var jobSwiper = new Swiper('.swiper-container', {
         loop: false,
         loopFillGroupWithBlank: true,
@@ -147,16 +126,10 @@ $(function () {
         $(this).addClass('active');
         jobSwiper.slideTo($(this).index(), 1000, false);
     });
-    
-    /*
-    * 监听滚动条，本来不想用jQuery但是发现js里面监听滚动条的事件不好添加，这边就引用了Jquery的$(obj).scroll();这个方法了
-    */
+
     $(window).scroll(function () {
-        //下面这句主要是获取网页的总高度，主要是考虑兼容性所以把Ie支持的documentElement也写了，这个方法至少支持IE8
         var htmlHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-        //clientHeight是网页在浏览器中的可视高度，
         var clientHeight = document.body.clientHeight || document.documentElement.clientHeight;
-        //scrollTop是浏览器滚动条的top位置，
         var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
         if (scrollTop >= 20) {
             $(".item1 .button").addClass("button_hover");
@@ -166,8 +139,6 @@ $(function () {
             $("header").removeClass("header_down");
         }
     })
-
-    // 按钮事件
     $(".item1 .button").hover(function () {
         $(".item1 .button").addClass("button_hover");
     })
--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 $(function () {
 
-    // 根据query参数跳至相应区块
     var blockName = getRequest().name || "";
 
     if(/Android|webOS|iPhone|iPod|iPad|BlackBerry/i.test(navigator.userAgent) && window.orientation === 0) {
-        // 手机端初始化
          $("header .delete").css('display', 'none');
          $("header .links").css('display', 'none');
      }
@@ -20,12 +18,10 @@ $(function () {
         })
     }
 
-    // 监听屏幕旋转
     $(window).bind('orientationchange', function(e){
         window.location.reload();
     });
 
-    // 获取query参数
     function getRequest() {
         var url = window.location.search; //获取url中"?"符后的字串
         var theRequest = new Object();
@@ -39,32 +35,27 @@ $(function () {
         return theRequest;
     }
 
-    // 展示menu
     function showMenu() {
         $("header .menu").css('display', 'block');
         $("header .links").css('display', 'none');
-        // 显示icon
         $("header .icon").animate({
             opacity: 1
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 显示menu
         $("header .menu").animate({
             opacity: 1
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 隐藏delete
         $("header .delete").animate({
             opacity: 0
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 隐藏links
         $("header .links").animate({
             opacity: 0
         }, {
@@ -76,34 +67,29 @@ $(function () {
         }, 600);
     }
 
-    // 隐藏menu
     function hideMenu() {
         $("header .delete").css('display', 'block');
         $("header .delete").css('opacity', 0);
         $("header .delete").css('z-index', 100);
         $("header .links").css('display', 'flex');
-        // 隐藏icon
         $("header .icon").animate({
             opacity: 0
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 隐藏menu
         $("header .menu").animate({
             opacity: 0
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 显示delete
         $("header .delete").animate({
             opacity: 1
         }, {
             duration: 600,
             easing: "swing"
         })
-        // 显示links
         $("header .links").animate({
             opacity: 1
         }, {
@@ -115,10 +101,8 @@ $(function () {
         }, 600);
     }
 
-    // 点击导航操作
     $(".link_item").click(function () {
         if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-            // 显示menu
             showMenu();
         }
         $("html, body").animate({
@@ -132,7 +116,6 @@ $(function () {
         return false;
     });
 
-    // 点击按钮
     $(".item1 .button").click(function () {
         $("html, body").animate({
             scrollTop: $("#block1")
@@ -145,19 +128,16 @@ $(function () {
         return false;
     });
 
-    // 手机端点击 右上角菜单
     $("header .menu").click(function () {
         hideMenu();
         return false;
     });
 
-    // 手机端点击 右上角关闭菜单
     $("header .delete").click(function () {
         showMenu();
         return false;
     });
 
-    // 获取屏幕可见区域高度
     function getClientHeight() {
         var clientHeight = 0;
         if (document.body.clientHeight && document.documentElement.clientHeight) {
@@ -172,7 +152,6 @@ $(function () {
         return clientHeight;
     }
 
-    // 初始化swiper
     var swiper1 = new Swiper('.swiper-container-t', {
         slidesPerView: 3,
         spaceBetween: 30,
@@ -197,15 +176,9 @@ $(function () {
         }
     });
 
-    /*
-    * 监听滚动条，本来不想用jQuery但是发现js里面监听滚动条的事件不好添加，这边就引用了Jquery的$(obj).scroll();这个方法了
-    */
     $(window).scroll(function () {
-        //下面这句主要是获取网页的总高度，主要是考虑兼容性所以把Ie支持的documentElement也写了，这个方法至少支持IE8
         var htmlHeight = document.body.scrollHeight || document.documentElement.scrollHeight;
-        //clientHeight是网页在浏览器中的可视高度，
         var clientHeight = document.body.clientHeight || document.documentElement.clientHeight;
-        //scrollTop是浏览器滚动条的top位置，
         var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
         if (scrollTop >= 20) {
             $(".item1 .button").addClass("button_hover");
@@ -214,8 +187,6 @@ $(function () {
             $(".item1 .button").removeClass("button_hover");
             $("header").removeClass("header_down");
         }
-
-        // 导航变色
         if ($('#block1').offset().top <= scrollTop && scrollTop < $('#block2').offset().top) {
             $(".links .first").addClass("link_item_active");
             $(".links .second").removeClass("link_item_active");
@@ -255,7 +226,6 @@ $(function () {
         }
     })
 
-    // 按钮事件
     $(".item1 .button").hover(function () {
         $(".item1 .button").addClass("button_hover");
     })
